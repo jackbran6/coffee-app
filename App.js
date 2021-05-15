2deefa3130586dfa79979507bbf1c9642577e285
Jack <jack.branthwaite@gmail.com>
2021-05-15 17:53:12 +1200

    Home page layout and stylings implemented

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
 import { TabScreens } from "./Navigation/TabNavigation/index";
 import { useFonts } from "expo-font";
 
+const navTheme = DefaultTheme;
+navTheme.colors.background = "#fff";
+
 const RootStack = createStackNavigator();
 const RootStackScreen = () => {
   return (
-    <RootStack.Navigator screenOptions={{ headerShown: false }}>
+    <RootStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      theme={navTheme}
+    >
       <RootStack.Screen name={"TabScreens"} component={TabScreens} />
     </RootStack.Navigator>
   );
@@ -24,8 +32,8 @@ export default function App() {
     return null;
   }
   return (
-    <NavigationContainer>
-      <RootStackScreen />
+    <NavigationContainer theme={navTheme}>
+      <RootStackScreen theme={navTheme} />
     </NavigationContainer>
   );
 }
